4f0badba7689d7900790580727c41819cc5a0bac
Christian Opitz <christian.opitz@netresearch.de>
2017-05-15 19:37:29 +0200

    Added frontend image rendering

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,2 +1,7 @@
 <?php
 defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+    'rte_ckeditor_image', 'setup',
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rte_ckeditor_image/Configuration/TypoScript/ImageRendering/setup.txt">'
+);
