ba50233b4254dc58a57fd4be4f9e71b44542c570
prdt3e <pr@network-media.de>
2020-05-01 04:53:17 +0200

    Add old transformation

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,13 @@ defined('TYPO3_MODE') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['softRefParser']['rtehtmlarea_images'] = \Netresearch\RteCKEditorImage\Database\RteImagesSoftReferenceIndex::class;
 
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_parsehtml_proc.php']['transformation']['rtehtmlarea_images_db'] = \Netresearch\RteCKEditorImage\Database\RteImagesDbHook::class;
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('RTE.default.proc.overruleMode := addToList(default)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('RTE.default.proc.overruleMode := addToList(rtehtmlarea_images_db)');
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-    'rte_ckeditor_image', 'setup',
+    'rte_ckeditor_image',
+    'setup',
     '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rte_ckeditor_image/Configuration/TypoScript/ImageRendering/setup.txt">'
 );
