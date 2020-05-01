47e393eb4951e1ffc8e169ba0c040686bf587998
prdt3e <pr@network-media.de>
2020-05-01 04:57:20 +0200

    add TS for rtehtmlarea_images_db and bootloader

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,18 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['softRefParser']['rtehtmlarea_images'] = \Netresearch\RteCKEditorImage\Database\RteImagesSoftReferenceIndex::class;
+$boot = function () {
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['softRefParser']['rtehtmlarea_images'] = \Netresearch\RteCKEditorImage\Database\RteImagesSoftReferenceIndex::class;
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_parsehtml_proc.php']['transformation']['rtehtmlarea_images_db'] = \Netresearch\RteCKEditorImage\Database\RteImagesDbHook::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_parsehtml_proc.php']['transformation']['rtehtmlarea_images_db'] = \Netresearch\RteCKEditorImage\Database\RteImagesDbHook::class;
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-    'rte_ckeditor_image', 'setup',
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rte_ckeditor_image/Configuration/TypoScript/ImageRendering/setup.txt">'
-);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('RTE.default.proc.overruleMode = rtehtmlarea_images_db');
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'rte_ckeditor_image', 'setup',
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rte_ckeditor_image/Configuration/TypoScript/ImageRendering/setup.txt">'
+    );
+};
+
+$boot();
+unset($boot);
