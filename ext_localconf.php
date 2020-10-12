10fc086b116bcf242e40cd5997700a1e7f584640
mcmulman <github@klickdienst.de>
2020-10-12 18:35:35 +0200

    [BUGFIX] #71: Fix tsconfig includes

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,9 +5,11 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['softRefParser']['rtehtmlare
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_parsehtml_proc.php']['transformation']['rtehtmlarea_images_db'] = \Netresearch\RteCKEditorImage\Database\RteImagesDbHook::class;
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('RTE.default.proc.overruleMode = rtehtmlarea_images_db');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('RTE.default.proc.overruleMode := addToList(default)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('RTE.default.proc.overruleMode := addToList(rtehtmlarea_images_db)');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-    'rte_ckeditor_image', 'setup',
+    'rte_ckeditor_image',
+    'setup',
     '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rte_ckeditor_image/Configuration/TypoScript/ImageRendering/setup.txt">'
 );
