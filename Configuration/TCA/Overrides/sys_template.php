4b3fdd2fb3b6eb452854dc4ef40ea544c71bb6fe
mcmulman <github@klickdienst.de>
2021-04-05 00:40:11 +0200

    [BUGFIX] #112: Remove wrapping p-tag from images via TS

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,4 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('rte_ckeditor_image', 'Configuration/TypoScript/ImageRendering', 'CKEditor Image Support');
