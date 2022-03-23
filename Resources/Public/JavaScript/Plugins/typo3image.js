7b3038bf26f55286744a9de403146371b97698d2
mabocke <102237713+mabocke@users.noreply.github.com>
2022-03-23 15:30:38 +0100

    Update typo3image.js, Fix Pull request #137

--- Resources/Public/JavaScript/Plugins/typo3image.js
+++ Resources/Public/JavaScript/Plugins/typo3image.js
@@ -166,6 +166,9 @@
                         var selection = editor.getSelection();
                         const selectedElement = selection.getSelectedElement();
                         if (selection.getSelectedText().trim() !== '' || selectedElement) {
+                            if (selectedElement && selectedElement.getName() === 'td'){
+                                return;
+                            }
                             element.setHtml(editor.getSelectedHtml(true));
                             var a = null;
                             if (selectedElement && selectedElement.getParent().getName() === 'a') {
