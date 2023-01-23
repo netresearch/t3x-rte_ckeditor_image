cba20d861c687fdda8f687aa088541efb26b6d94
Rico Sonntag <rico.sonntag@netresearch.de>
2023-01-23 13:53:31 +0000

    Rework RteImagesSoftReferenceIndex to fix phpstan issues

--- Classes/Database/RteImagesSoftReferenceIndex.php
+++ Classes/Database/RteImagesSoftReferenceIndex.php
@@ -11,49 +11,43 @@ declare(strict_types=1);
 
 namespace Netresearch\RteCKEditorImage\Database;
 
-use Psr\EventDispatcher\EventDispatcherInterface;
 use TYPO3\CMS\Core\DataHandling\SoftReference\AbstractSoftReferenceParser;
 use TYPO3\CMS\Core\DataHandling\SoftReference\SoftReferenceParserResult;
 use TYPO3\CMS\Core\Html\HtmlParser;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
- * Class for processing of the FAL soft references on img tags inserted in RTE content
+ * Class for processing of the FAL soft references on img tags inserted in RTE content.
  *
- * Based on
- * @link https://gitlab.sgalinski.de/typo3/tinymce4_rte/blob/513eeadf8c3c7ffba0936ad63b24e1e9c2eccba7/Classes/Hook/SoftReferenceHook.php
- *
- *
- * @category   Netresearch
- * @package    RteCKEditor
- * @subpackage Database
- * @author     Stefan Galinski <stefan@sgalinski.de>
- * @license    http://www.gnu.de/documents/gpl-2.0.de.html GPL 2.0+
- * @link       http://www.netresearch.de
+ * @author  Stefan Galinski <stefan@sgalinski.de>
+ * @license http://www.gnu.de/documents/gpl-2.0.de.html GPL 2.0+
+ * @link    http://www.netresearch.de
  */
 class RteImagesSoftReferenceIndex extends AbstractSoftReferenceParser
 {
     /**
-     * Content split into images and other elements
-     *
-     * @var array<int, string>
-     */
-    protected array $splittedContentTags = [];
-
-    /**
-     * TYPO3 HTML Parser
+     * TYPO3 HTML parser.
      *
      * @var HtmlParser
      */
-    protected HtmlParser $htmlParser;
+    private HtmlParser $htmlParser;
 
     /**
-     * @var EventDispatcherInterface
+     * @var array<int, string>
      */
-    protected EventDispatcherInterface $eventDispatcher;
+    private array $splitContentTags;
 
     /**
-     * Main function through which all processing happens
+     * Constructor.
+     *
+     * @param HtmlParser $htmlParser
+     */
+    public function __construct(HtmlParser $htmlParser)
+    {
+        $this->htmlParser = $htmlParser;
+    }
+
+    /**
+     * Main function through which all processing happens.
      *
      * @param string $table         Database table name
      * @param string $field         Field name for which processing occurs
@@ -61,101 +55,111 @@ class RteImagesSoftReferenceIndex extends AbstractSoftReferenceParser
      * @param string $content       The content/value of the field
      * @param string $structurePath If running from inside a FlexForm structure, this is the path of the tag.
      *
-     * @return SoftReferenceParserResult Result array on positive matches. Otherwise FALSE
+     * @return SoftReferenceParserResult
      */
-    public function parse(string $table, string $field, int $uid, string $content, string $structurePath = ''): SoftReferenceParserResult
-    {
-        $this->setTokenIdBasePrefix($table, (string)$uid, $field, $structurePath);
+    public function parse(
+        string $table,
+        string $field,
+        int $uid,
+        string $content,
+        string $structurePath = ''
+    ): SoftReferenceParserResult {
+        $this->setTokenIdBasePrefix($table, (string) $uid, $field, $structurePath);
 
         if ($this->parserKey === 'rtehtmlarea_images') {
-            $retVal = $this->findRef_rtehtmlarea_images($content);
+            $retVal = $this->findImageTags($content);
         } else {
             $retVal = [];
         }
 
-        return SoftReferenceParserResult::create(
-          $content,
-          is_array($retVal) ? $retVal : []
-        );
+        return SoftReferenceParserResult::create($content, $retVal);
     }
 
     /**
-     * Parses Content
-     * Finds image tags with data-htmlarea-file-uid attribute in the content.
-     * All images that have a data-htmlarea-file-uid attribute will be returned with an info text
+     * Finds image tags with "data-htmlarea-file-uid" attribute in the content. All images that have
+     * a "data-htmlarea-file-uid" attribute will be returned with an info text.
      *
      * @param string $content The input content to analyse
      *
-     * @return array{content: string, elements: array<string, array{matchString: string, subst: array{type: string, recordRef: string, tokenID: string, tokenValue: mixed}}>}|boolean  Result array on positive matches, see description above. Otherwise FALSE
+     * @return array<string, string|array<string, array<string, string|array<string, mixed>>>>
      */
-    public function findRef_rtehtmlarea_images(string $content)
+    private function findImageTags(string $content): array
     {
-        $retVal = false;
-        // Start HTML parser and split content by image tag
-        $this->htmlParser = GeneralUtility::makeInstance(HtmlParser::class);
-        $this->splittedContentTags = $this->htmlParser->splitTags('img', $content);
+        // Content split into images and other elements
+        $this->splitContentTags = $this->htmlParser->splitTags(
+            'img',
+            $content
+        );
 
         $images = $this->findImagesWithDataUid();
 
-        // Assemble result array
-        if (count($images) > 0) {
-            $retVal = [
-                'content' => implode('', $this->splittedContentTags),
-                'elements' => $images
-            ];
+        if (count($images) === 0) {
+            return [];
         }
 
-        return $retVal;
+        // Assemble result array
+        return [
+            'content'  => implode('', $this->splitContentTags),
+            'elements' => $images,
+        ];
     }
 
     /**
-     * Checks for image tags
+     * Checks for image tags.
      *
      * @param string $element
      *
      * @return bool
      */
-    private function hasImageTag(string $element): bool
+    private function isImageTag(string $element): bool
     {
-        $pattern = "/^<img/";
-        return (bool)preg_match($pattern, $element);
+        return (bool) preg_match('/^<img/', $element);
     }
 
     /**
-     * Finding image tags with data-htmlarea-file-uid attribute in the content.
-     * All images that have a data-htmlarea-file-uid attribute will be returned with an info text
+     * Finding image tags with "data-htmlarea-file-uid" attribute in the content. All images that have
+     * a "data-htmlarea-file-uid" attribute will be returned with an info text.
      *
-     * @return array<string, array{matchString: string, subst: array{type: string, recordRef: string, tokenID: string, tokenValue: mixed}}>
+     * @return array<array<string, mixed>>
      */
     private function findImagesWithDataUid(): array
     {
         $images = [];
 
         // Traverse split parts
-        foreach ($this->splittedContentTags as $k => $v) {
-
-            if ($this->hasImageTag($v)) {
-
-                // Get FAL uid reference
-                $attribs = $this->htmlParser->get_tag_attributes($v);
-                $fileUid = $attribs[0]['data-htmlarea-file-uid'] ?? false;
-
-                // If there is a file uid, continue. Otherwise, ignore this img tag.
-                if ($fileUid) {
-                    // Initialize the element entry with info text here
-                    $tokenID = $this->makeTokenID((string) $k);
-                    $images[$k] = [];
-                    $images[$k]['matchString'] = $v;
-                    // Token and substitute value
-                    $this->splittedContentTags[$k] = str_replace('data-htmlarea-file-uid="' . $fileUid . '"', 'data-htmlarea-file-uid="{softref:' . $tokenID . '}"', $this->splittedContentTags[$k]);
-                    $images[$k]['subst'] = [
-                        'type' => 'db',
-                        'recordRef' => 'sys_file:' . $fileUid,
-                        'tokenID' => $tokenID,
-                        'tokenValue' => $fileUid
-                    ];
-                }
+        foreach ($this->splitContentTags as $key => $htmlTag) {
+            if (!$this->isImageTag($htmlTag)) {
+                continue;
             }
+
+            // Get FAL uid reference
+            $attribs = $this->htmlParser->get_tag_attributes($htmlTag);
+            $fileUid = $attribs[0]['data-htmlarea-file-uid'] ?? false;
+
+            // If there is a file uid, continue. Otherwise, ignore this img tag.
+            if ($fileUid === false) {
+                continue;
+            }
+
+            // Initialize the element entry with info text here
+            $tokenID = $this->makeTokenID((string) $key);
+
+            $images[$key] = [];
+            $images[$key]['matchString'] = $htmlTag;
+
+            // Token and substitute value
+            $this->splitContentTags[$key] = str_replace(
+                'data-htmlarea-file-uid="' . $fileUid . '"',
+                'data-htmlarea-file-uid="{softref:' . $tokenID . '}"',
+                $htmlTag
+            );
+
+            $images[$key]['subst'] = [
+                'type'       => 'db',
+                'recordRef'  => 'sys_file:' . $fileUid,
+                'tokenID'    => $tokenID,
+                'tokenValue' => $fileUid,
+            ];
         }
 
         return $images;
