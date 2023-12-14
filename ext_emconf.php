686083f7bdcf73737ba9fd134d30713923d3fde2
Gitsko <s.koschel@hotmail.de>
2023-12-14 10:52:00 +0100

    [TASK]  Update ext_emconf.php for TYPO3 v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,9 +11,9 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '12.0.0',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.9.99',
-            'typo3' => '11.0.0-12.4.99',
-            'rte_ckeditor' => '11.0.0-12.4.99',
+            'php' => '8.1.0-8.9.99',
+            'typo3' => '12.4.0-12.4.99',
+            'rte_ckeditor' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [
