b956f854f5e24c40837dd76183183a856ffa5d32
Sybille Peters <sypets@gmx.de>
2024-02-23 21:22:32 +0100

    Fix functional test for v13

--- Tests/Functional/DataHandling/RteImageSoftReferenceParserTest.php
+++ Tests/Functional/DataHandling/RteImageSoftReferenceParserTest.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 namespace Netresearch\RteCKEditorImage\Tests\Functional\DataHandling;
 
 use TYPO3\CMS\Core\Database\ReferenceIndex;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 class RteImageSoftReferenceParserTest extends FunctionalTestCase
@@ -13,11 +15,20 @@ class RteImageSoftReferenceParserTest extends FunctionalTestCase
 
     /**
      * @test
+     *
+     * @todo remove version switch when v12 support is dropped
      */
     public function updateReferenceIndexAddsIndexEntryForImage(): void
     {
         $this->importCSVDataSet(__DIR__ . '/Fixtures/ReferenceIndex/UpdateReferenceIndexImport.csv');
-        (new ReferenceIndex())->updateIndex(false);
+
+        $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+        if ($versionInformation->getMajorVersion() < 13) {
+            (new ReferenceIndex())->updateIndex(false);
+        } else {
+            $this->get(ReferenceIndex::class)->updateIndex(false);
+        }
+
         $this->assertCSVDataSet(__DIR__ . '/Fixtures/ReferenceIndex/UpdateReferenceIndexResult.csv');
     }
 }
