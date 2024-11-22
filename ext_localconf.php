a58c4d4b4f014babf560c8596269916f93f10515
Sebastian Mendel <sebastian.mendel@netresearch.de>
2024-11-22 11:09:14 +0000

    chore: remove deprecated usage of ExtensionManagementUtility::addPageTSConfig()

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,16 +13,3 @@ defined('TYPO3') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][]
     = \Netresearch\RteCKEditorImage\Database\RteImagesDbHook::class;
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    'RTE.default.proc.overruleMode := addToList(default)
-    
-    RTE.default.buttons.image.options.magic {
-        maxWidth = 1920
-        maxHeight = 9999
-    }
-    '
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    'RTE.default.proc.overruleMode := addToList(rtehtmlarea_images_db)'
-);
