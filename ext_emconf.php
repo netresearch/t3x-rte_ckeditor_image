1adaac2e277e34abfb3fb3b6c728ddb488fd6c6a
Rico Sonntag <rico.sonntag@netresearch.de>
2025-02-05 13:36:21 +0000

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,22 +1,31 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
-    'title' => 'CKEditor Rich Text Editor Image Support',
-    'description' => 'Adds FAL image support to CKEditor for TYPO3.',
-    'category' => 'be',
-    'state' => 'stable',
-    'clearCacheOnLoad' => 0,
-    'author' => 'Sebastian Koschel',
-    'author_email' => 'sebastian.koschel@netresearch.de',
-    'version' => '13.0.0',
-    'constraints' => [
-        'depends' => [
-            'php' => '8.2.0-8.9.99',
-            'typo3' => '13.4.0-13.4.99',
+/**
+ * This file is part of the package netresearch/nr-sync.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+$EM_CONF['rte_ckeditor_image'] = [
+    'title'            => 'CKEditor Rich Text Editor Image Support',
+    'description'      => 'Adds FAL image support to CKEditor for TYPO3.',
+    'category'         => 'be',
+    'author'           => 'Sebastian Koschel, Sebastian Mendel, Rico Sonntag',
+    'author_email'     => 'sebastian.koschel@netresearch.de, sebastian.mendel@netresearch.de, rico.sonntag@netresearch.de',
+    'author_company' => 'Netresearch DTT GmbH',
+    'state'            => 'stable',
+    'version'          => '13.0.0',
+    'constraints'      => [
+        'depends'   => [
+            'php'          => '8.2.0-8.9.99',
+            'typo3'        => '13.4.0-13.4.99',
             'rte_ckeditor' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
-        'suggests' => [
+        'suggests'  => [
             'setup' => '',
         ],
     ],
