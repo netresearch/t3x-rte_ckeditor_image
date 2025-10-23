10baf030747bfab08747539e2374757a26779ed0
Sebastian Mendel <info@sebastianmendel.de>
2025-10-23 18:41:53 +0200

    feat: add global Page TSConfig loading for automatic configuration
    Create ext_tables.php to load Configuration/page.tsconfig globally for
    all sites using ExtensionManagementUtility::addPageTSConfig(). This
    eliminates the need for manual site-specific page.tsconfig files.
    
    With this change, the RTE preset is automatically applied to all sites
    after extension installation.

---
+++ ext_tables.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the package netresearch/rte-ckeditor-image.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+defined('TYPO3') || exit;
+
+// Load Page TSConfig globally for all sites
+ExtensionManagementUtility::addPageTSConfig(
+    '@import "EXT:rte_ckeditor_image/Configuration/page.tsconfig"'
+);
