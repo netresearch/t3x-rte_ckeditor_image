3cb0ed67e092720d6951fc18469b16661cedf3eb
Sebastian Mendel <github@sebastianmendel.de>
2025-11-26 12:59:56 +0100

    feat: update branding to mention Netresearch
    - Update descriptions in composer.json and ext_emconf.php to mention Netresearch
    - Add complete author information with company details
    - Add authors array to composer.json with homepage links
    - Update version to 13.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,13 +2,13 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'CKEditor Rich Text Editor Image Support',
-    'description' => 'Adds FAL image support to CKEditor for TYPO3.',
+    'description' => 'Adds FAL image support to CKEditor for TYPO3 - by Netresearch.',
     'category' => 'be',
     'state' => 'stable',
-    'clearCacheOnLoad' => 0,
-    'author' => 'Sebastian Koschel',
-    'author_email' => 'sebastian.koschel@netresearch.de',
-    'version' => '13.0.0',
+    'author' => 'Sebastian Koschel, Sebastian Mendel, Rico Sonntag',
+    'author_email' => 'sebastian.koschel@netresearch.de, sebastian.mendel@netresearch.de, rico.sonntag@netresearch.de',
+    'author_company' => 'Netresearch DTT GmbH',
+    'version' => '13.0.1',
     'constraints' => [
         'depends' => [
             'php' => '8.2.0-8.9.99',
